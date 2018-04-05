6404020521d5a78b32a68d42efe24209ee35c513
Milan SINGH <msingh37@ds2.CS.McGill.CA>
2018-04-05 09:23:44 -0400

    Triangle Mesh intersection now implemented using Moller-Trumbore method

--- src/comp557/a4/Mesh.java
+++ src/comp557/a4/Mesh.java
@@ -1,7 +1,13 @@
 package comp557.a4;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import comp557.a4.PolygonSoup.Vertex;
 
 public class Mesh extends Intersectable {
 	
@@ -20,12 +26,109 @@ public class Mesh extends Intersectable {
 		super();
 		this.soup = null;
 	}			
+	
+	private final static double EPSILON = 1e-6;
 		
 	@Override
-	public void intersect(Ray ray, IntersectResult result) {
-		
+	public void intersect(Ray ray, IntersectResult result) 
+	{
+		System.out.println("");
 		// TODO: Objective 7: ray triangle intersection for meshes
+		List<int[]> faces = soup.faceList;
+		List<Vertex> verts = soup.vertexList;
+		Vector3d[] faceNormals = new Vector3d[ faces.size() ];
 		
+		double t = Double.POSITIVE_INFINITY;
+		for( int faceIdx = 0; faceIdx < faces.size(); faceIdx++ )
+		{
+			
+			int[] face = faces.get( faceIdx );
+			
+			if( face.length != 3 )
+			{
+				System.out.println( "Non triangle mesh" );
+				return;
+			}
+			
+			Vertex[] faceVerts = new Vertex[ face.length ];
+			for( int i = 0; i < face.length; i++ )
+			{
+				faceVerts[ i ] = verts.get( face[ i ] );
+			}
+			
+			// use Möller-Trumbore algorithm to detect intersection
+			Vector3d e0 = new Vector3d( faceVerts[2].p );
+			e0.sub( faceVerts[0].p );
+			
+			Vector3d e1 = new Vector3d( faceVerts[1].p );
+			e1.sub( faceVerts[0].p );
+			
+			Vector3d h = new Vector3d();
+			h.cross( ray.viewDirection, e1 );
+			
+			double a = e0.dot( h );
+			if( a > -EPSILON && a < EPSILON ) //If h is not perpendicular to e0 i.e. lies on another plane
+				continue;
+			
+			double f = 1/a;
+			
+			Vector3d s = new Vector3d( ray.eyePoint );
+			s.sub( faceVerts[ 0 ].p );
+			
+			double u = f * s.dot( h );
+			if( u < 0.0 || u > 1.0 )
+				continue;
+			
+			Vector3d q = new Vector3d();
+			q.cross( s, e0 );
+			
+			double v = f * ray.viewDirection.dot( q );
+			if( v < 0.0 || u + v > 1.0 )
+				continue;
+			
+			t = f * e1.dot( q );
+			if( t > EPSILON && t < result.t )
+			{
+				result.t = t;
+				ray.getPoint( t, result.p );
+				result.material = material;
+				calcFaceNormal( faceVerts, result.n );
+			}
+			else
+				continue;
+		}
 	}
 
+	private static void calcFaceNormal( Vertex[] faceVerts, Vector3d normalOut )
+	{
+		if( normalOut == null )
+			return;
+		
+		Vector3d v = new Vector3d( faceVerts[2].p );
+		v.sub( faceVerts[0].p );
+		
+		Vector3d u = new Vector3d( faceVerts[1].p );
+		u.sub( faceVerts[0].p );
+		
+		normalOut.cross( u, v );
+		
+	}
+	
+	private static double intersectPlane( Ray ray, Vector3d n )
+	{
+		Vector3d invEye = new Vector3d( ray.eyePoint );
+    	invEye.scale( -1.0 );
+        
+    	n.normalize();
+        double t = invEye.dot( n )/ray.viewDirection.dot( n );
+		
+		return t;
+	}
+	
+	private static boolean pointBoundedByFace( Point3d p, Vertex[] face )
+	{
+		
+		
+		return false;
+	}
 }
